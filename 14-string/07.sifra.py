a22481b4a600d6f392f005ebbf08eb1a48268ed1
Samuel Smreček <smreceksamo@gmail.com>
2025-03-25 20:49:31 +0100

    cviguhb

--- 14-string/07.sifra.py
+++ 14-string/07.sifra.py
@@ -1,6 +1,35 @@
-slovo=input("zadaj slovo: ")
-for i in slovo:
-    c1=ord(i)
-    c1+=1
-    c2=chr(c1)
-    print(c2)
+while True:
+    print("[1] pre zasifrovanie")
+    print("[2] pre rozsifrovanie")
+    print("[3] pre ukoncenie")
+
+    choise = input("Vyber cislo: ")
+    text2 = ""
+
+    if choise == "1":
+        text = input("Zadaj text: ")
+        text = text.lower()
+        for i in text:
+            char = ord(i)+1
+            if char == 123:
+                char = 97
+            text2 += chr(char)
+        print("Zasifrovany text: ",text2)
+        print("----------------")
+
+    elif choise == "2":
+        text = input("Zadaj text: ")
+        text = text.lower()
+        for i in text:
+            char = ord(i)-1
+            if char == 96:
+                char = 122
+            text2 += chr(char)
+        print("Rozsifrovany text:",text2)
+        print("----------------")
+        
+    elif choise == "3":
+        break
+    else:
+        print("zla volba")
+        print("----------------")
